563d658911ff0b6246e91a3c6c8074dc4ca249cd
Sam <42203151+samgladstone@users.noreply.github.com>
2020-11-22 22:12:36 +0000

    Add a better countdown

--- src/sections/birthday/Countdown.vue
+++ src/sections/birthday/Countdown.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>{{counter}}</div>
+    <transition name="count-down">
+        <div :key="counter">{{counter}}</div>
+    </transition>
 </template>
 
 <script>
@@ -27,4 +29,21 @@ export default {
 </script>
 
 <style scoped>
+div {
+    font-size: 8em;
+}
+
+/* Enter and leave animations can use different */
+/* durations and timing functions.              */
+.count-down-enter-active {
+  transition: all .3s ease;
+}
+.count-down-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.count-down-enter, .count-down-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  font-size: 0;
+  opacity: 0;
+}
 </style>
